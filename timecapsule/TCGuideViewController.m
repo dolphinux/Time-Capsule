d09d35f275d7db98504c9bbfc1538dafb62e67e2
dolphinux <dolphinux@gmail.com>
2013-11-18 18:55:45 +0800

    设定首页动画页内容

---
+++ timecapsule/TCGuideViewController.m
@@ -0,0 +1,38 @@
+//
+//  TCGuideViewController.m
+//  timecapsule
+//
+//  Created by dolphinux on 13-11-18.
+//  Copyright (c) 2013年 com.dolphinux. All rights reserved.
+//
+
+#import "TCGuideViewController.h"
+
+@interface TCGuideViewController ()
+
+@end
+
+@implementation TCGuideViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
